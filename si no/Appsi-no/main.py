500f899d2554b834fba8a05eb909ad5b9ae7433f
alilm12345 <alilm061023@gmail.com>
2024-09-05 19:15:02 -0600

    se subio la version 1.2 de si no

--- si no/Appsi-no/main.py
+++ si no/Appsi-no/main.py
@@ -2,7 +2,31 @@ import flet as ft
 
 
 def main(page: ft.Page):
-    page.add(ft.SafeArea(ft.Text("Hello, Flet!")))
+    page.title = "¿me perdonas?"
+    page.bgcolor="pink"
+    page.horizontal_aligment=ft.CrossAxisAlignment.center()
+    page.vertical_aligment=ft.MainAxisAlignment.CENTER
+    
+    lbl1 = ft.text("¿me perdonas?",
+                    style=fty.textStyle(size=40,veight="bold"))
+    img1=ft.image(src="triste.png",width=200,height=200)
+    
+    btnSi=ft.ElevatedButton("si",
+                            color="green",
+                            width=100,
+                            height=50
+                            )
+    page.add(
+        ft.column(
+            (
+                lbl1,
+                img1,
+                ft.Row(
+                    (btnSi,btnNo)
+                )
+            )
+        )
+    )
 
 
 ft.app(main)
